a291cba3899890d17823744fab7cb64abfde8e3f
Aruan.Kapbasov <aruanfreedom@outlook.com>
2016-11-01 16:19:38 +0600

    add increase and decrease

--- js/main.js
+++ js/main.js
@@ -5,4 +5,30 @@ $( document ).ready(function() {
         console.log("ok");
     });
 
+function resizeText(multiplier) {
+    var max = 1.1,
+        min = 0.9;
+
+  if (document.body.style.fontSize === "") {
+    document.body.style.fontSize = "1.0em";
+  }
+//   console.log(multiplier);
+    if(parseFloat(document.body.style.fontSize) === max || parseFloat(document.body.style.fontSize) === min) {
+        document.body.style.fontSize = "1em";
+    }
+
+  if (parseFloat(document.body.style.fontSize) < max && parseFloat(document.body.style.fontSize) > min) {
+      document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.1) + "em";
+  }  
+  console.log( parseFloat(document.body.style.fontSize) );
+}
+
+$('#increase').on('click', function () {
+    resizeText(1);
+});
+
+$('#decrease').on('click', function () {
+    resizeText(-1);
+});
+
 });
